e6dfcd2978a59084eaaa28cfaee5fefe8defafc3
David Brown <dtbrown@deloitte.co.uk>
2022-06-09 09:34:19 +0100

    footer added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-//import Footer from "./components/Footer/Footer";
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/pages/home/Home';
 //import Dashboard from "./components/Dashboard/Dashboard";
@@ -17,7 +17,7 @@ function App() {
 						{/* <Route path="/dashboard" element={<Dashboard />} /> */}
 					</Routes>
 				</div>
-				{/* <Footer /> */}
+				<Footer />
 			</Router>
 		</div>
 	);
