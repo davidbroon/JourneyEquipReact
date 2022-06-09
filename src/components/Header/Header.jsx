4809d90e8af060963634d34449f7b2b777999a02
David Brown <dtbrown@deloitte.co.uk>
2022-06-09 21:40:41 +0100

    journey equip icon added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 import { HeaderStyle } from './Header-Style';
-import logo from '../../assets/images/whiteLogoJE.png';
+import logo from '../../assets/images/JE Logo-02.svg';
 import { NavLink, Link } from 'react-router-dom';
 import { Link as LinkScroll } from 'react-scroll';
 
